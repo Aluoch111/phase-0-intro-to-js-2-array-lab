412bd080418200c89674177f335bd98d60a7f5cb
Grace Deborah Aluoch <gracedeborah377@gmail.com>
2022-08-14 00:46:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+// const newCats = [...cats];
+function appendCat(){
+ const newCats=[...cats,"Broom"];
+  return newCats;
+}
+function prependCat(){
+    const otherCats=["Arnold",...cats];
+    return otherCats;
+}
+function removeLastCat(){
+    const lastCat =cats.slice(0,-1);
+    return lastCat;
+}
+function removeFirstCat(){
+    const firstCat = cats.slice(1);
+    return firstCat;
+}
